088659cb4708ceed6b770eed2d02d99aa8257e38
Astrow-xz <matias.parada15@inacapmail.cl>
2026-04-14 14:18:08 -0400

    avance en el codigo

--- hola 2.py
+++ hola 2.py
@@ -3,4 +3,12 @@ edad = int(input("¿Cuál es tu edad? "))
 año_actual = 2025
 año_nacimiento = año_actual - edad
 print("Naciste en el año " + str(año_nacimiento) + ".")
-#ola
+
+#segun año de nacimiento ya calculado calcular descuentos
+if año_nacimiento <= 2000:
+    print("Tienes un descuento del 20% en tu compra.")
+elif año_nacimiento > 2000 and año_nacimiento <= 2010:
+    print("Tienes un descuento del 10% en tu compra.")
+else:
+    print("No tienes descuento en tu compra.")
+    
