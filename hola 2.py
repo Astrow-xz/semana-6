96d9456ac3919d95cfb0df9c7fcce6c5a7046897
Astrow-xz <matias.parada15@inacapmail.cl>
2026-04-14 13:45:05 -0400

    se agrego ejemplo 2

---
+++ hola 2.py
@@ -0,0 +1,5 @@
+#codigo calcula fecha de nacimiento 
+edad = int(input("¿Cuál es tu edad? "))
+año_actual = 2025
+año_nacimiento = año_actual - edad
+print("Naciste en el año " + str(año_nacimiento) + ".")
